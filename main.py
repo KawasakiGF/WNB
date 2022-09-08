2fa40d9512c5f6a33fb6d1c43697dddaf3053ca1
kawasakiGF <zxcy-1m4g1n3@docomo.ne.jp>
2022-09-08 16:12:26 +0900

    pic test

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        ImageSendMessage(ogirinal_content_url='https://www.dropbox.com/s/hnqi5gjgb9b3wmf/Sun.jpg?dl=0',preview_content_url='https://www.dropbox.com/s/hnqi5gjgb9b3wmf/Sun.jpg?dl=0'))
+        ImageSendMessage(ogirinal_content_url='https://1.bp.blogspot.com/-eaDZ7sDP9uY/Xhwqlve5SUI/AAAAAAABXBo/EcI2C2vim7w2WV6EYy3ap0QLirX7RPohgCNcBGAsYHQ/s400/pose_syanikamaeru_man.png',preview_content_url='https://1.bp.blogspot.com/-eaDZ7sDP9uY/Xhwqlve5SUI/AAAAAAABXBo/EcI2C2vim7w2WV6EYy3ap0QLirX7RPohgCNcBGAsYHQ/s400/pose_syanikamaeru_man.png'))
 
 #関数名は自由
 #def handle_message(event):
