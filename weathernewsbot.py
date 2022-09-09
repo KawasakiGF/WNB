af69646d76076fc286c3b48257cf690dfdf5e7be
kawasakiGF <zxcy-1m4g1n3@docomo.ne.jp>
2022-09-09 14:36:28 +0900

    bot prog test

--- weathernewsbot.py
+++ weathernewsbot.py
@@ -112,18 +112,18 @@ def callback():
 
 ##########実行するプログラムの内容をここに書く################
 #@handler.addのメソッドの引数にはイベントのモデルを入れる(MessageEvent=メッセージを受けたら)
-if TextMessage in "天気":
-  @handler.add(MessageEvent)
+@handler.add(MessageEvent,message=TextMessage)
 #関数名は自由
+if TextMessage in "天気":
   def handle_message(event):
       line_bot_api.reply_message(
           event.reply_token,
           [TextSendMessage(text=tenkiInfo),
           ImageSendMessage(original_content_url=picUrl,preview_image_url=picUrl),
           TextSendMessage(text=fukusou)])
-          #リプライはLineBotApiのメソッドを用いる。 第一引数のevent.reply_tokenはイベントの応答に
-          #用いるトークン。 第二引数にはlinebot.modelsに定義されている返信用の
-          #TextSendMessageオブジェクトを渡しています。
+        #リプライはLineBotApiのメソッドを用いる。 第一引数のevent.reply_tokenはイベントの応答に
+        #用いるトークン。 第二引数にはlinebot.modelsに定義されている返信用の
+         #TextSendMessageオブジェクトを渡しています。
 ##############################################
 
 #決まり文句
